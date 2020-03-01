2f21de1a7018733d1a13a164b4ede24a68f10fab
Daniel Solyom <solyomd@gmail.com>
2020-03-01 11:51:54 +0100

    splits research to per item technology

--- prototypes/technology/technology.lua
+++ prototypes/technology/technology.lua
@@ -2,24 +2,96 @@ data:extend(
 {
   {
     type = "technology",
-    name = "advanced-equipment",
-    icon = "__advanced-equipment__/graphics/technology/advanced-equipment.png",
+    name = "power-armor-mk3",
+    icon = "__advanced-equipment__/graphics/technology/power-armor-mk3.png",
 	  icon_size = 128,
-    prerequisites = {"power-armor-mk2", "battery-mk2-equipment", "energy-shield-mk2-equipment", "fusion-reactor-equipment", "speed-module-3", "effectivity-module-3"},
+    prerequisites = {"power-armor-mk2", "speed-module-3", "effectivity-module-3"},
     effects =
     {
       {
         type = "unlock-recipe",
         recipe = "power-armor-mk3"
+      }
+    },
+    unit =
+    {
+      count = 600,
+      ingredients =
+      {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"military-science-pack", 1},
+        {"chemical-science-pack", 1},
+        {"production-science-pack", 1},
+        {"utility-science-pack", 1},
       },
+      time = 30
+    },
+    order = "g-i-b"
+  },
+  {
+    type = "technology",
+    name = "battery-mk3-equipment",
+    icon = "__advanced-equipment__/graphics/technology/battery-mk3-equipment.png",
+	  icon_size = 128,
+    prerequisites = {"battery-mk2-equipment", "utility-science-pack"},
+    effects =
+    {
       {
         type = "unlock-recipe",
         recipe = "battery-mk3-equipment"
+      }
+    },
+    unit =
+    {
+      count = 150,
+      ingredients =
+      {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
+        {"utility-science-pack", 1},
       },
+      time = 30
+    },
+    order = "g-i-b"
+  },
+  {
+    type = "technology",
+    name = "energy-shield-mk3-equipment",
+    icon = "__advanced-equipment__/graphics/technology/energy-shield-mk3-equipment.png",
+	  icon_size = 128,
+    prerequisites = {"energy-shield-mk2-equipment", "military-4"},
+    effects =
+    {
       {
         type = "unlock-recipe",
         recipe = "energy-shield-mk3-equipment"
+      }
+    },
+    unit =
+    {
+      count = 250,
+      ingredients =
+      {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"military-science-pack", 1},
+        {"chemical-science-pack", 1},
+        {"utility-science-pack", 1},
       },
+      time = 30
+    },
+    order = "g-i-b"
+  },
+  {
+    type = "technology",
+    name = "fusion-reactor-mk2-equipment",
+    icon = "__advanced-equipment__/graphics/technology/fusion-reactor-mk2-equipment.png",
+	  icon_size = 128,
+    prerequisites = {"fusion-reactor-equipment", "production-science-pack"},
+    effects =
+    {
       {
         type = "unlock-recipe",
         recipe = "fusion-reactor-mk2-equipment"
@@ -27,7 +99,7 @@ data:extend(
     },
     unit =
     {
-      count = 600,
+      count = 400,
       ingredients =
       {
         {"automation-science-pack", 1},
